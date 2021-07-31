5d12a80f09627e6fe92c77a903f1ecc29fdbf797
hai <hai@LAPTOP-4DV9N6FJ>
2021-07-31 17:51:54 +0530

    Ninth commit
    Upload all files

---
+++ inheritance/src/inheritance/company.java
@@ -0,0 +1,21 @@
+package inheritance;
+
+public class company {
+	String company_feature;
+
+	public String getCompany_feature() {
+		return company_feature;
+	}
+
+	public void setCompany_feature(String company_feature) {
+		this.company_feature = company_feature;
+	}
+}
+	
+		
+
+	
+
+	
+
+
---
+++ inheritance/src/inheritance/mobile.java
@@ -0,0 +1,12 @@
+package inheritance;
+
+public class mobile {
+
+	public static void main(String[] args) {
+		Samsung s=new Samsung();
+		s.setCompany_feature("Company high mega pixel");
+		System.out.println("samsung company feature:"+s.getCompany_feature());
+
+	}
+
+}
